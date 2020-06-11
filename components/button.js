6158bb2f0de17e3988fffd85b4886246c8e21e4a
Jonathan Steele <jonathan@steele.pro>
2020-06-11 11:54:03 +0100

    feat: Add <Button /> component

--- components/button.js
+++ components/button.js
@@ -1,9 +1,45 @@
 import Link from 'next/link'
+import cx from 'classnames'
 
-function Button({ href, label }) {
+function Button({ href, label, style = 'SOLID', theme }) {
+  const styleVal = (style) => {
+    switch (style) {
+      case 'OUTLINE':
+      case 'SOLID':
+        return style
+      default:
+        return 'SOLID'
+    }
+  }
+
+  const themeVal = (theme) => {
+    switch (theme) {
+      case 'BLUE':
+        return colour
+      default:
+        return 'BLUE'
+    }
+  }
+
+  const buttonClass = {
+    SOLID: {
+      BLUE: 'border-transparent bg-blue-600 text-white',
+    },
+    OUTLINE: {
+      BLUE: 'border-blue-600 text-blue-600',
+    },
+  }
+
   return (
     <Link href={href || '/'}>
-      <a>{label}</a>
+      <a
+        className={cx(
+          'border w-full flex items-center justify-center px-8 py-3 text-base leading-6 font-medium rounded-md focus:outline-none focus:shadow-outline md:py-4 md:text-lg md:px-10',
+          buttonClass[styleVal(style)][themeVal(theme)]
+        )}
+      >
+        {label}
+      </a>
     </Link>
   )
 }
