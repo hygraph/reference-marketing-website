69a6ed0db639d18c6bc3110da64485d53384326a
Jonathan Steele <jonathan.steele@graphcms.com>
2020-06-16 16:12:59 +0100

    feat: Add <Grid /> component (#9)
    * refactor: Deduplication of page query
    
    * feat: Add <Grid /> component

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { graphcmsClient } from '../lib/_client'
+import { pageQuery } from '../lib/_queries'
 import Wrapper from '../components/wrapper'
 
 function IndexPage({ page }) {
@@ -6,78 +7,7 @@ function IndexPage({ page }) {
 }
 
 export async function getStaticProps() {
-  const { page } = await graphcmsClient.request(
-    `query IndexPageQuery($slug: String!) {
-      page(where: {slug: $slug}) {
-        banner {
-          id
-          content
-          href
-          slug
-          theme
-        }
-        blocks {
-          __typename
-          ... on Grid {
-            id
-            columns {
-              __typename
-              ... on BlogPost {
-                id
-                authors {
-                  name
-                }
-                content {
-                  markdown
-                }
-                coverImage {
-                  id
-                  title
-                  url
-                }
-                published
-                slug
-                title
-              }
-              ... on Feature {
-                id
-                content {
-                  markdown
-                }
-                image {
-                  id
-                  title
-                  url
-                }
-                slug
-                title
-              }
-            }
-            slug
-          }
-          ... on Hero {
-            id
-            buttons {
-              id
-              href
-              label
-              style
-            }
-            image {
-              id
-              title
-              url
-            }
-            slug
-          }
-        }
-        id
-        subtitle
-        title
-      }
-    }`,
-    { slug: 'home' }
-  )
+  const { page } = await graphcmsClient.request(pageQuery, { slug: 'home' })
 
   return {
     props: {
