14221e2c67fa3bee159e49f5d615f8b29fb41ce2
Jonathan Steele <jonathan@steele.pro>
2020-06-23 10:17:57 +0100

    feat: Add featured post

--- pages/blog.js
+++ pages/blog.js
@@ -2,7 +2,7 @@ import { BlogPostCard } from '../components/blocks/columns'
 import { graphcmsClient } from '../lib/_client'
 import { blogQuery } from '../lib/_queries'
 
-function BlogPage({ page: { subtitle, title }, posts }) {
+function BlogPage({ featuredPosts, page: { subtitle, title }, posts }) {
   return (
     <React.Fragment>
       <main className="mt-10 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 xl:mt-28">
@@ -15,20 +15,28 @@ function BlogPage({ page: { subtitle, title }, posts }) {
           </p>
         </div>
       </main>
-      <div className="grid gap-14 grid-cols-1 lg:grid-cols-3">
-        {posts.map((post) => (
-          <BlogPostCard key={post.id} {...post} />
-        ))}
+      <div className="max-w-xl mx-auto px-4 py-8 sm:py-12 lg:py-20 sm:px-6 lg:px-8 lg:max-w-screen-xl">
+        <div className="grid gap-14 grid-cols-1 mb-14">
+          {featuredPosts.map((post) => (
+            <BlogPostCard key={post.id} {...post} isFeatured />
+          ))}
+        </div>
+        <div className="grid gap-14 grid-cols-1 lg:grid-cols-3">
+          {posts.map((post) => (
+            <BlogPostCard key={post.id} {...post} />
+          ))}
+        </div>
       </div>
     </React.Fragment>
   )
 }
 
 export async function getStaticProps() {
-  const { page, posts } = await graphcmsClient.request(blogQuery)
+  const { featuredPosts, page, posts } = await graphcmsClient.request(blogQuery)
 
   return {
     props: {
+      featuredPosts,
       page,
       posts,
     },
