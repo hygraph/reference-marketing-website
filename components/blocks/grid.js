6c1f80c26e9d09901ab2fb8f8366a9b7e39a71b4
Jonathan Steele <jonathan@steele.pro>
2020-08-25 12:31:12 +0100

    fix: <Grid /> background colour

--- components/blocks/grid.js
+++ components/blocks/grid.js
@@ -37,31 +37,28 @@ function Grid({
   }
 
   return (
-    <div
-      className={cx(
-        'relative max-w-xl mx-auto px-4 py-8 sm:py-12 lg:py-20 sm:px-6 lg:px-8 lg:max-w-screen-xl',
-        gridThemeClass(theme)
-      )}
-    >
-      <div className="relative mb-8 lg:mb-16 text-center">
-        <Heading
-          level={3}
-          className={cx({
-            'mb-4': gridSubtitle,
+    <div className={gridThemeClass(theme)}>
+      <div className="relative max-w-xl mx-auto px-4 py-8 sm:py-12 lg:py-20 sm:px-6 lg:px-8 lg:max-w-screen-xl">
+        <div className="relative mb-8 lg:mb-16 text-center">
+          <Heading
+            level={3}
+            className={cx({
+              'mb-4': gridSubtitle,
+            })}
+          >
+            {gridTitle}
+          </Heading>
+          {gridSubtitle && <MarkdownRenderer content={gridSubtitle.markdown} />}
+        </div>
+        <div className={cx('grid gap-14', colWidthClass(width))}>
+          {columns.map((column, index) => {
+            const Component = Columns[component] || Columns[column.__typename]
+
+            if (!Component) return null
+
+            return <Component key={index} {...column} />
           })}
-        >
-          {gridTitle}
-        </Heading>
-        {gridSubtitle && <MarkdownRenderer content={gridSubtitle.markdown} />}
-      </div>
-      <div className={cx('grid gap-14', colWidthClass(width))}>
-        {columns.map((column, index) => {
-          const Component = Columns[component] || Columns[column.__typename]
-
-          if (!Component) return null
-
-          return <Component key={index} {...column} />
-        })}
+        </div>
       </div>
     </div>
   )
