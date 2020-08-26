c9e7bf31dff2d6f3b02d5f6607ef9848a2413a24
Jonathan Steele <jonathan@steele.pro>
2020-08-26 11:12:31 +0100

    fix: Don't wrap MDX in <p>

--- components/blocks/columns/feature.js
+++ components/blocks/columns/feature.js
@@ -10,7 +10,7 @@ function Feature({ content, image, title }) {
     <div className="lg:grid lg:grid-flow-row-dense lg:grid-cols-2 lg:gap-8 lg:items-center">
       <div className="lg:col-start-2">
         <Heading level={4}>{title}</Heading>
-        <p className="mt-3 text-lg leading-7 text-gray-500">{mdxContent}</p>
+        {mdxContent}
       </div>
       <div className="mt-10 -mx-4 lg:mx-0 relative lg:mt-0 lg:col-start-1">
         <img
