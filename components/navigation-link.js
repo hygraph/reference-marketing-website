4edc15a83e883e18ac4d3e66ffdfced0175cde30
Jonathan Steele <jonathan@steele.pro>
2020-09-03 16:12:33 +0100

    feat: Add active styling for <NavigationLink />

--- components/navigation-link.js
+++ components/navigation-link.js
@@ -1,15 +1,32 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+import cx from 'classnames'
 
 function NavigationLink({ slug }) {
+  const router = useRouter()
+
+  const isActive = router.asPath.startsWith(`/${slug}`)
   const isStaticPage = ['blog', 'home'].includes(slug)
 
   return isStaticPage ? (
     <Link href={`/${slug}`}>
-      <a>{slug}</a>
+      <a
+        className={cx('font-medium', {
+          'text-red-300': isActive,
+        })}
+      >
+        {slug}
+      </a>
     </Link>
   ) : (
     <Link href="/[slug]" as={`/${slug}`}>
-      <a>{slug}</a>
+      <a
+        className={cx('font-medium', {
+          'text-red-300': isActive,
+        })}
+      >
+        {slug}
+      </a>
     </Link>
   )
 }
