be42aaece617830bedd0813b9ec283e6d460fb73
Jonathan Steele <jonathan@steele.pro>
2020-09-03 13:12:24 +0100

    feat: Add basic navigation to <Wrapper /> component

--- components/wrapper.js
+++ components/wrapper.js
@@ -1,10 +1,27 @@
+import Link from 'next/link'
+
 import Banner from '../components/banner'
 import * as Blocks from '../components/blocks'
 
-function Wrapper({ banner, blocks, ...page }) {
+function Wrapper({ banner, blocks, navigation, ...page }) {
   return (
     <React.Fragment>
       {banner && <Banner {...banner} />}
+      {navigation.pages.length && (
+        <ul>
+          {navigation.pages.map((page) => {
+            const linkHref = page.slug === 'home' ? '/' : page.slug
+
+            return (
+              <li key={page.id}>
+                <Link href={linkHref}>
+                  <a>{page.slug}</a>
+                </Link>
+              </li>
+            )
+          })}
+        </ul>
+      )}
       {blocks.map((block, index) => {
         const Component = Blocks[block.__typename]
 
