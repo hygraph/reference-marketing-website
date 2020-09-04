764b83d9ecc053e6363adef94f34d35bb841af33
Jonathan Steele <jonathan@steele.pro>
2020-09-04 13:06:57 +0100

    feat: Design for <Navigation />

--- components/navigation-link.js
+++ components/navigation-link.js
@@ -7,6 +7,7 @@ function NavigationLink({ slug }) {
 
   const isActive = router.asPath.startsWith(`/${slug}`)
   const isStaticPage = ['blog', 'home'].includes(slug)
+  const linkLabel = slug.charAt(0).toUpperCase() + slug.slice(1)
 
   return (
     <Link
@@ -14,11 +15,14 @@ function NavigationLink({ slug }) {
       as={isStaticPage ? null : `/${slug}`}
     >
       <a
-        className={cx('font-medium', {
-          'text-red-300': isActive,
-        })}
+        className={cx(
+          'font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out',
+          {
+            'text-blue-600': isActive,
+          }
+        )}
       >
-        {slug}
+        {linkLabel}
       </a>
     </Link>
   )
--- components/navigation.js
+++ components/navigation.js
@@ -4,20 +4,45 @@ import NavigationLink from './navigation-link'
 
 function Navigation({ pages }) {
   return (
-    <ul>
-      <li>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-      </li>
-      {pages.map((page, index) => {
-        return (
-          <li key={index}>
-            <NavigationLink {...page} />
-          </li>
-        )
-      })}
-    </ul>
+    <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
+      <nav className="relative flex items-center justify-between sm:h-10 lg:justify-start">
+        <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
+          <div className="flex items-center justify-between w-full md:w-auto">
+            <Link href="/">
+              <a aria-label="Home">Home</a>
+            </Link>
+            <div className="-mr-2 flex items-center md:hidden">
+              <button
+                type="button"
+                className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
+                id="main-menu"
+                aria-label="Main menu"
+                aria-haspopup="true"
+              >
+                <svg
+                  className="h-6 w-6"
+                  stroke="currentColor"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="hidden md:block md:ml-10 md:pr-4 space-x-8">
+          {pages.map((page, index) => {
+            return <NavigationLink key={index} {...page} />
+          })}
+        </div>
+      </nav>
+    </div>
   )
 }
 
