4abcccbb25a9241ceee263184ff94ac785864693
Jonathan Steele <jonathan@steele.pro>
2020-09-10 09:45:11 +0100

    fix: Only show mobile navigation button if there's pages

--- components/navigation.js
+++ components/navigation.js
@@ -58,30 +58,32 @@ function Navigation({ pages }) {
                   <LogoSVG className="h-10 w-auto" />
                 </a>
               </Link>
-              <div className="-mr-2 flex items-center md:hidden">
-                <button
-                  type="button"
-                  className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
-                  id="main-menu"
-                  aria-label="Main menu"
-                  aria-haspopup="true"
-                  onClick={() => setMobileNavOpen(true)}
-                >
-                  <svg
-                    className="h-6 w-6"
-                    stroke="currentColor"
-                    fill="none"
-                    viewBox="0 0 24 24"
+              {pages && pages.length ? (
+                <div className="-mr-2 flex items-center md:hidden">
+                  <button
+                    type="button"
+                    className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
+                    id="main-menu"
+                    aria-label="Main menu"
+                    aria-haspopup="true"
+                    onClick={() => setMobileNavOpen(true)}
                   >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M4 6h16M4 12h16M4 18h16"
-                    />
-                  </svg>
-                </button>
-              </div>
+                    <svg
+                      className="h-6 w-6"
+                      stroke="currentColor"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M4 6h16M4 12h16M4 18h16"
+                      />
+                    </svg>
+                  </button>
+                </div>
+              ) : null}
             </div>
           </div>
           {pages && pages.length ? (
