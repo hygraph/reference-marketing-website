2036d074b0ae7764f0fdc7bf3df57380eb3e9014
Jonathan Steele <jonathan@steele.pro>
2020-12-01 12:10:33 +0000

    refactor: Use <Transition /> component from @headlessui/react

--- components/navigation.js
+++ components/navigation.js
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { Transition } from '@tailwindui/react'
+import { Transition } from '@headlessui/react'
 
 import LogoSVG from '../svg/logo.svg'
 import NavigationLink from './navigation-link'
