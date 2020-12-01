d443ebee549a79cc802bac696a5b7e3f95f27eac
Jonathan Steele <jonathan@steele.pro>
2020-12-01 12:21:25 +0000

    feat: Add _locales.js to extend locales config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
+const { locales } = require('./lib/_locales')
+
 module.exports = {
   i18n: {
-    defaultLocale: 'en',
-    locales: ['de', 'en']
+    defaultLocale: locales.find((locale) => locale.default).value,
+    locales: locales.map((locale) => locale.value)
   },
   images: {
     domains: ['media.graphcms.com']
