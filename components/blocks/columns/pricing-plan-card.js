17fc6fcccc848079e1923d245504aa05b8737692
Jonathan Steele <jonathan@steele.pro>
2020-12-15 15:12:00 +0000

    refactor: <CheckIcon /> component

--- components/blocks/columns/pricing-plan-card.js
+++ components/blocks/columns/pricing-plan-card.js
@@ -1,3 +1,5 @@
+import { CheckIcon } from '../../icons'
+
 function PricingPlanCard({
   annualPrice,
   billingPeriod,
@@ -37,19 +39,7 @@ function PricingPlanCard({
         <ul className="mt-6 space-y-4">
           {included.map((feature, index) => (
             <li key={index} className="flex space-x-3">
-              <svg
-                className="flex-shrink-0 h-5 w-5 text-green-500"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                  clip-rule="evenodd"
-                />
-              </svg>
+              <CheckIcon className="flex-shrink-0 h-5 w-5 text-green-500" />
               <span className="text-sm text-gray-500">{feature}</span>
             </li>
           ))}
