0a13e911a67b1ba8bc0bc0df651ce19c0c3f8af6
Jonathan Steele <jonathan@steele.pro>
2020-12-18 13:00:09 +0000

    feat: <Button /> updates

--- components/button.js
+++ components/button.js
@@ -1,78 +1,45 @@
 import Link from 'next/link'
 import cx from 'classnames'
 
-function Button({
-  href,
-  label,
-  size = 'REGULAR',
-  style = 'SOLID',
-  theme = 'BLUE'
-}) {
-  const styleVal = (style) => {
-    switch (style) {
-      case 'OUTLINE':
-      case 'SOLID':
-        return style
-      default:
-        return 'SOLID'
-    }
-  }
-
+function Button({ href, label, theme = 'BLUE' }) {
   const themeVal = (theme) => {
     switch (theme) {
-      case 'BLUE':
       case 'WHITE':
-        return theme
+        return 'border-transparent bg-white text-indigo-600 hover:bg-gray-50'
+      case 'BLUE':
       default:
-        return 'BLUE'
-    }
-  }
-
-  const buttonClass = {
-    SOLID: {
-      BLUE: 'border-transparent bg-blue-600 text-white',
-      WHITE: 'border-transparent bg-white text-gray-900'
-    },
-    OUTLINE: {
-      BLUE: 'border-blue-600 text-blue-600'
-    }
-  }
-
-  const sizeClass = (size) => {
-    switch (size) {
-      case 'SMALL':
-        return 'px-4 py-2 rounded text-sm md:text-base'
-      default:
-      case 'REGULAR':
-        return 'leading-6 px-8 py-3 rounded-md text-base md:py-4 md:text-lg md:px-10'
+        return 'border-transparent bg-indigo-600 text-white hover:bg-indigo-700'
     }
   }
 
   const linkClass = cx(
-    'border w-full flex items-center justify-center font-medium focus:outline-none focus:shadow-outline',
-    buttonClass[styleVal(style)][themeVal(theme)],
-    sizeClass(size)
+    'w-full flex items-center justify-center px-8 py-3 border text-base font-medium rounded-md md:py-4 md:text-lg md:px-10',
+    themeVal(theme)
   )
 
   if (!href || !label) return null
 
   if (href.includes('http')) {
     return (
-      <a
-        className={linkClass}
-        href={href}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        {label}
-      </a>
+      <div className="rounded-md shadow">
+        <a
+          className={linkClass}
+          href={href}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {label}
+        </a>
+      </div>
     )
   }
 
   return (
-    <Link href={href}>
-      <a className={linkClass}>{label}</a>
-    </Link>
+    <div className="rounded-md shadow">
+      <Link href={href}>
+        <a className={linkClass}>{label}</a>
+      </Link>
+    </div>
   )
 }
 
