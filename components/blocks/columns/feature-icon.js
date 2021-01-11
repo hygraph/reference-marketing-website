167047d8c81874ac520ec9ff8bd4c075e05f14bd
Jonathan Steele <jonathan.steele@graphcms.com>
2021-01-11 11:59:15 +0000

    feat: Add <FeatureSection /> component (#61)
    * chore(deps): Add lodash.{camelcase,startcase} dependencies
    
    * feat: Add new SVG icons
    
    * feat: Add <FeatureIcon /> component
    
    * feat: Add <FeatureSection /> component

---
+++ components/blocks/columns/feature-icon.js
@@ -0,0 +1,26 @@
+import hydrate from 'next-mdx-remote/hydrate'
+import camelCase from 'lodash.camelcase'
+import startCase from 'lodash.startcase'
+
+import * as Icons from '../../icons'
+
+function FeatureIcon({ content, icon, title }) {
+  const mdxContent = hydrate(content.mdx)
+
+  const IconComponent =
+    Icons[`${startCase(camelCase(icon))}Icon`] || Icons.DefaultIcon
+
+  return (
+    <div>
+      <div className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+        <IconComponent className="h-6 w-6" aria-hidden="true" />
+      </div>
+      <div className="mt-5">
+        <dt className="text-lg leading-6 font-medium text-gray-900">{title}</dt>
+        <dd className="mt-2 text-base text-gray-500">{mdxContent}</dd>
+      </div>
+    </div>
+  )
+}
+
+export default FeatureIcon
