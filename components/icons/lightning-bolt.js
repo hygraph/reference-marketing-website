477ffdcc6f183d3ad71e9e6bb619521cdb38e023
Jonathan Steele <jonathan@steele.pro>
2021-01-11 11:55:36 +0000

    feat: Add new SVG icons

---
+++ components/icons/lightning-bolt.js
@@ -0,0 +1,20 @@
+function LightningBolt({ ...props }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      {...props}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M13 10V3L4 14h7v7l9-11h-7z"
+      />
+    </svg>
+  )
+}
+
+export default LightningBolt
