4bc535fb2d14591a5cc1c887057bb630794d8e50
Jonathan Steele <jonathan@steele.pro>
2021-01-12 13:14:06 +0000

    refactor: Tidy util functions

--- utils/_parseBlocksMdx.js
+++ utils/_parseBlocksMdx.js
@@ -5,35 +5,25 @@ import { parseColumnsMdx } from '@/utils/_parseColumnsMdx'
 
 const parseBlocksMdx = async (blocks) =>
   await Promise.all(
-    blocks.map(async (block) => {
-      switch (block.__typename) {
-        case 'Grid':
-        case 'Testimonial':
-          return parseGridMdx(block)
-        default:
-          return block
-      }
-    })
+    blocks.map(async ({ columns, content, gridSubtitle, ...block }) => ({
+      ...(gridSubtitle && {
+        gridSubtitle: {
+          markdown: gridSubtitle,
+          mdx: await renderToString(he.decode(gridSubtitle))
+        }
+      }),
+      ...(content && {
+        content: {
+          markdown: content,
+          mdx: await renderToString(he.decode(content))
+        }
+      }),
+      ...block,
+      ...(columns &&
+        columns.length && {
+          columns: await parseColumnsMdx(columns)
+        })
+    }))
   )
-
-const parseGridMdx = async ({ columns, content, gridSubtitle, ...block }) => ({
-  ...(gridSubtitle && {
-    gridSubtitle: {
-      markdown: gridSubtitle,
-      mdx: await renderToString(he.decode(gridSubtitle))
-    }
-  }),
-  ...(content && {
-    content: {
-      markdown: content,
-      mdx: await renderToString(he.decode(content))
-    }
-  }),
-  ...block,
-  ...(columns &&
-    columns.length && {
-      columns: await parseColumnsMdx(columns)
-    })
-})
 
 export { parseBlocksMdx }
--- utils/_parseColumnsMdx.js
+++ utils/_parseColumnsMdx.js
@@ -3,26 +3,15 @@ import he from 'he'
 
 const parseColumnsMdx = async (columns) =>
   await Promise.all(
-    columns.map(async (column) => {
-      switch (column.__typename) {
-        case 'BlogPost':
-        case 'Faq':
-        case 'Feature':
-          return parseContentMdx(column)
-        default:
-          return column
-      }
-    })
+    columns.map(async ({ content, ...column }) => ({
+      ...(content && {
+        content: {
+          markdown: content,
+          mdx: await renderToString(he.decode(content))
+        }
+      }),
+      ...column
+    }))
   )
-
-const parseContentMdx = async ({ content, ...column }) => ({
-  ...(content && {
-    content: {
-      markdown: content,
-      mdx: await renderToString(he.decode(content))
-    }
-  }),
-  ...column
-})
 
 export { parseColumnsMdx }
