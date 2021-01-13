256fbb456c84a31f222e3f732dd272bd6cd8fdc8
Jonathan Steele <jonathan@steele.pro>
2021-01-13 10:04:31 +0000

    feat: Add <DefaultSeo /> to _app.js

---
+++ next-seo.config.js
@@ -0,0 +1,21 @@
+const title = 'Build Modern Marketing Websites with a Headless CMS'
+const description =
+  'Learn how to build modern marketing websites, with localization and SEO, using GraphCMS, NextJS, Tailwind, and Vercel.'
+
+const SEO = {
+  title,
+  titleTemplate: '%s | GraphCMS',
+  description,
+  openGraph: {
+    type: 'website',
+    url: 'https://marketing-websites.withheadlesscms.com',
+    title,
+    description
+  },
+  twitter: {
+    handle: '@GraphCMS',
+    site: '@GraphCMS'
+  }
+}
+
+export default SEO
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,21 @@
+import * as React from 'react'
+import { DefaultSeo } from 'next-seo'
+
 import { SiteLayout } from '@/layout'
 
+import SEO from '../next-seo.config'
 import 'tailwindcss/tailwind.css'
 
 function App({ Component, pageProps }) {
   const getLayout =
     Component.getLayout || ((page) => <SiteLayout>{page}</SiteLayout>)
 
-  return getLayout(<Component {...pageProps} />)
+  return (
+    <React.Fragment>
+      <DefaultSeo {...SEO} />
+      {getLayout(<Component {...pageProps} />)}
+    </React.Fragment>
+  )
 }
 
 export default App
