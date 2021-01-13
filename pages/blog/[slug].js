1ec543077a63f9a63927ca910f0f7637889345c2
Jonathan Steele <jonathan@steele.pro>
2021-01-13 10:51:49 +0000

    feat: Add <SEO /> component

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,3 +1,4 @@
+import * as React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
@@ -8,6 +9,7 @@ import { blogPostQuery } from '@/lib/_queries'
 import { getContentLayout } from '@/layout'
 import { graphcmsClient } from '@/lib/_client'
 import { parsePostData } from '@/utils/_parsePostData'
+import SEO from '@/components/seo'
 
 function BlogPost({ nextPost, post, previousPost }) {
   const router = useRouter()
@@ -19,104 +21,109 @@ function BlogPost({ nextPost, post, previousPost }) {
   const mdxContent = hydrate(post.content.mdx)
 
   return (
-    <article className="relative max-w-xl mx-auto px-4 py-8 sm:py-12 lg:py-20 sm:px-6 lg:px-8 lg:max-w-screen-xl">
-      <header className="pt-6 lg:pb-10">
-        <div className="space-y-1 text-center">
-          <dl className="space-y-10">
+    <React.Fragment>
+      <SEO {...post.seo} />
+      <article className="relative max-w-xl mx-auto px-4 py-8 sm:py-12 lg:py-20 sm:px-6 lg:px-8 lg:max-w-screen-xl">
+        <header className="pt-6 lg:pb-10">
+          <div className="space-y-1 text-center">
+            <dl className="space-y-10">
+              <div>
+                <dt className="sr-only">Published on</dt>
+                <dd className="text-base leading-6 font-medium text-gray-500">
+                  <time dateTime={post.published}>
+                    {post.formattedPublished}
+                  </time>
+                </dd>
+              </div>
+            </dl>
             <div>
-              <dt className="sr-only">Published on</dt>
-              <dd className="text-base leading-6 font-medium text-gray-500">
-                <time dateTime={post.published}>{post.formattedPublished}</time>
-              </dd>
+              <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
+                {post.title}
+              </h1>
             </div>
+          </div>
+        </header>
+        <div
+          className="divide-y lg:divide-y-0 divide-gray-200 lg:grid lg:grid-cols-4 lg:gap-x-6 pb-16 lg:pb-20"
+          style={{ gridTemplateRows: 'auto 1fr' }}
+        >
+          <dl className="pt-6 pb-10 lg:pt-11 lg:border-b lg:border-gray-200">
+            <dt className="sr-only">Author</dt>
+            <dd>
+              <ul className="flex justify-center lg:block space-x-8 sm:space-x-12 lg:space-x-0 lg:space-y-8">
+                {post.authors.map((author) => (
+                  <li key={author.id} className="flex items-center space-x-2">
+                    <div className="h-10 relative w-10">
+                      <Image
+                        className="rounded-full"
+                        src={author.photo.url}
+                        alt={author.name}
+                        title={author.name}
+                        layout="fill"
+                      />
+                    </div>
+                    <dl className="flex-1 text-sm font-medium leading-5">
+                      <dt className="sr-only">Name</dt>
+                      <dd className="text-gray-900">{author.name}</dd>
+                    </dl>
+                  </li>
+                ))}
+              </ul>
+            </dd>
           </dl>
-          <div>
-            <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
-              {post.title}
-            </h1>
+          <div className="divide-y divide-gray-200 lg:pb-0 lg:col-span-3 lg:row-span-2">
+            {post.coverImage && (
+              <Image
+                className="object-cover mx-auto rounded-lg shadow-md"
+                src={post.coverImage.url}
+                alt={post.coverImage.title}
+                title={post.coverImage.title}
+                height={post.coverImage.height}
+                width={post.coverImage.width}
+              />
+            )}
+            <div className="prose max-w-none pt-10 pb-8">{mdxContent}</div>
           </div>
-        </div>
-      </header>
-      <div
-        className="divide-y lg:divide-y-0 divide-gray-200 lg:grid lg:grid-cols-4 lg:gap-x-6 pb-16 lg:pb-20"
-        style={{ gridTemplateRows: 'auto 1fr' }}
-      >
-        <dl className="pt-6 pb-10 lg:pt-11 lg:border-b lg:border-gray-200">
-          <dt className="sr-only">Author</dt>
-          <dd>
-            <ul className="flex justify-center lg:block space-x-8 sm:space-x-12 lg:space-x-0 lg:space-y-8">
-              {post.authors.map((author) => (
-                <li key={author.id} className="flex items-center space-x-2">
-                  <div className="h-10 relative w-10">
-                    <Image
-                      className="rounded-full"
-                      src={author.photo.url}
-                      alt={author.name}
-                      title={author.name}
-                      layout="fill"
-                    />
+          <footer className="text-sm font-medium leading-5 divide-y divide-gray-200 lg:col-start-1 lg:row-start-2">
+            {(nextPost || previousPost) && (
+              <div className="space-y-8 py-8">
+                {nextPost && (
+                  <div>
+                    <h2 className="text-xs tracking-wide uppercase text-gray-500">
+                      Next Post
+                    </h2>
+                    <div className="text-indigo-500 hover:text-indigo-600">
+                      <Link href={`/blog/${nextPost.slug}`}>
+                        <a>{nextPost.title}</a>
+                      </Link>
+                    </div>
                   </div>
-                  <dl className="flex-1 text-sm font-medium leading-5">
-                    <dt className="sr-only">Name</dt>
-                    <dd className="text-gray-900">{author.name}</dd>
-                  </dl>
-                </li>
-              ))}
-            </ul>
-          </dd>
-        </dl>
-        <div className="divide-y divide-gray-200 lg:pb-0 lg:col-span-3 lg:row-span-2">
-          {post.coverImage && (
-            <Image
-              className="object-cover mx-auto rounded-lg shadow-md"
-              src={post.coverImage.url}
-              alt={post.coverImage.title}
-              title={post.coverImage.title}
-              height={post.coverImage.height}
-              width={post.coverImage.width}
-            />
-          )}
-          <div className="prose max-w-none pt-10 pb-8">{mdxContent}</div>
-        </div>
-        <footer className="text-sm font-medium leading-5 divide-y divide-gray-200 lg:col-start-1 lg:row-start-2">
-          {(nextPost || previousPost) && (
-            <div className="space-y-8 py-8">
-              {nextPost && (
-                <div>
-                  <h2 className="text-xs tracking-wide uppercase text-gray-500">
-                    Next Post
-                  </h2>
-                  <div className="text-indigo-500 hover:text-indigo-600">
-                    <Link href={`/blog/${nextPost.slug}`}>
-                      <a>{nextPost.title}</a>
-                    </Link>
+                )}
+                {previousPost && (
+                  <div>
+                    <h2 className="text-xs tracking-wide uppercase text-gray-500">
+                      Previous Post
+                    </h2>
+                    <div className="text-indigo-500 hover:text-indigo-600">
+                      <Link href={`/blog/${previousPost.slug}`}>
+                        <a>{previousPost.title}</a>
+                      </Link>
+                    </div>
                   </div>
-                </div>
-              )}
-              {previousPost && (
-                <div>
-                  <h2 className="text-xs tracking-wide uppercase text-gray-500">
-                    Previous Post
-                  </h2>
-                  <div className="text-indigo-500 hover:text-indigo-600">
-                    <Link href={`/blog/${previousPost.slug}`}>
-                      <a>{previousPost.title}</a>
-                    </Link>
-                  </div>
-                </div>
-              )}
+                )}
+              </div>
+            )}
+            <div className="pt-8">
+              <Link href="/blog">
+                <a className="text-indigo-500 hover:text-indigo-600">
+                  &larr; Back to the blog
+                </a>
+              </Link>
             </div>
-          )}
-          <div className="pt-8">
-            <Link href="/blog">
-              <a className="text-indigo-500 hover:text-indigo-600">
-                &larr; Back to the blog
-              </a>
-            </Link>
-          </div>
-        </footer>
-      </div>
-    </article>
+          </footer>
+        </div>
+      </article>
+    </React.Fragment>
   )
 }
 
