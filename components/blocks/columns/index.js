211ccbf574063a2bfcf7ff062045feccae2756c5
Jonathan Steele <jonathan@steele.pro>
2021-01-15 14:25:19 +0000

    refactor: Consolidate <FAQSection /> into <Grid />

--- components/blocks/columns/index.js
+++ components/blocks/columns/index.js
@@ -1,4 +1,5 @@
 export { default as BlogPostCard } from './blog-post-card'
+export { default as FAQCard } from './faq-card'
 export { default as FeatureCheck } from './feature-check'
 export { default as FeatureIcon } from './feature-icon'
 export { default as PricingPlanCard } from './pricing-plan-card'
--- components/blocks/index.js
+++ components/blocks/index.js
@@ -1,5 +1,4 @@
 export { default as Breakpoint } from './breakpoint'
-export { default as FAQSection } from './faq-section'
 export { default as Grid } from './grid'
 export { default as LogoCloud } from './logo-cloud'
 export { default as PricingPlanSection } from './pricing-plan-section'
