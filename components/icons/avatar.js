bcf48cc39bae761f65d1e163e1d88bc4626dac63
Jonathan Steele <jonathan@steele.pro>
2021-01-22 11:41:42 +0000

    fix: Fallback if person photo is null

---
+++ components/icons/avatar.js
@@ -0,0 +1,14 @@
+function Avatar({ ...props }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      {...props}
+    >
+      <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+    </svg>
+  )
+}
+
+export default Avatar
